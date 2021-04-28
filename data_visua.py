3a6e5b7385c4deec833bd2a46cdd320c81ab562f
shenthebest1 <78027134+shenthebest1@users.noreply.github.com>
2021-04-29 00:36:36 +0800

    Update data_visua.py

--- data_visua.py
+++ data_visua.py
@@ -72,3 +72,111 @@ ax.spines['left'].set_visible(True)
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 plt.savefig("Percentage plot")
+
+
+
+
+
+#Create numerical variables
+
+dat['bmi_cat'] = pd.cut(dat['bmi'], bins = [0, 19, 25,30,10000], labels = ['Underweight', 'Ideal', 'Overweight', 'Obesity'])
+dat['age_cat'] = pd.cut(dat['age'], bins = [0,13,18, 45,60,200], labels = ['Children', 'Teens', 'Adults','Mid Adults','Elderly'])
+dat['glucose_cat'] = pd.cut(dat['avg_glucose_level'], bins = [0,90,160,230,500], labels = ['Low', 'Normal', 'High', 'Very High'])
+
+
+
+
+
+'''plot Heart stroke and age graph'''
+fig = plt.figure(figsize = (24,10), dpi = 60)
+
+gt = fig.add_gridspec(10,24)
+gt.update(wspace = 1, hspace = 0.05)
+
+
+ax1 = fig.add_subplot(gt[1:10,13:]) 
+ax2 = fig.add_subplot(gt[1:4,0:8]) 
+ax3 = fig.add_subplot(gt[6:9, 0:8]) 
+
+
+# set up axes list
+axes = [ ax1,ax2, ax3]
+
+
+# setting of axes
+for ax in axes:
+    ax.axes.get_yaxis().set_visible(False)
+    ax.set_facecolor('#f6f5f5')
+    
+    for loc in ['left', 'right', 'top', 'bottom']:
+        ax.spines[loc].set_visible(False)
+
+fig.patch.set_facecolor('#f6f5f5')
+        
+ax1.axes.get_xaxis().set_visible(False)
+ax1.axes.get_yaxis().set_visible(True)
+
+
+
+stroke_age = dat[dat['stroke'] == 1].age_cat.value_counts()
+healthy_age = dat[dat['stroke'] == 0].age_cat.value_counts()
+
+ax1.hlines(y = ['Children', 'Teens', 'Adults', 'Mid Adults', 'Elderly'], xmin = [644,270,1691,1129,1127], 
+          xmax = [1,1,11,59,177], color = 'grey',**{'linewidth':0.5})
+
+
+sns.scatterplot(y = stroke_age.index, x = stroke_age.values, s = stroke_age.values*2, color = '#343bfe', ax= ax1, alpha = 1)
+sns.scatterplot(y = healthy_age.index, x = healthy_age.values, s = healthy_age.values*2, color = '#e6a129', ax= ax1, alpha = 1)
+
+ax1.axes.get_xaxis().set_visible(False)
+ax1.set_xlim(xmin = -500, xmax = 2250)
+ax1.set_ylim(ymin = -1,ymax = 5)
+
+ax1.set_yticklabels( labels = ['Children', 'Teens', 'Adults', 'Mid Adults', 'Elderly'],fontdict = {'font':'Serif', 'fontsize':16,'fontweight':'bold', 'color':'black'})
+
+ax1.text(-950,5.8, 'How Age Impact on Heart Strokes' ,{'font': 'Serif', 'Size': '25','weight':'bold', 'color':'black'},alpha = 0.9)
+ax1.text(1000,4.8, 'Stroke ', {'font': 'Serif','weight':'bold','Size': '16','weight':'bold','style':'normal', 'color':'#343bfe'})
+ax1.text(1300,4.8, '|', {'color':'black' , 'size':'16', 'weight': 'bold'})
+ax1.text(1350,4.8, 'Healthy', {'font': 'Serif','weight':'bold', 'Size': '16','style':'normal', 'weight':'bold','color':'#e6a129'})
+ax1.text(-950,5., 'Age have significant association with stokes, older people have larger probability of getting strokes \nmid age adults are the second higest', 
+        {'font':'Serif', 'size':'16','color': 'black'})
+
+ax1.text(stroke_age.values[0] + 30,4.05, stroke_age.values[0], {'font':'Serif', 'Size':14, 'weight':'bold', 'color':'#343bfe'})
+ax1.text(healthy_age.values[2] - 300,4.05, healthy_age.values[2], {'font':'Serif', 'Size':14, 'weight':'bold', 'color':'#e6a129'})
+
+ax1.text(stroke_age.values[1] + 30,3.05, stroke_age.values[1], {'font':'Serif', 'Size':14, 'weight':'bold', 'color':'#343bfe'})
+ax1.text(healthy_age.values[1] - 300,3.05, healthy_age.values[1], {'font':'Serif', 'Size':14, 'weight':'bold', 'color':'#e6a129'})
+
+
+# plot the distribution plots 
+
+sns.kdeplot(data = dat, x = 'age', ax = ax2, shade = True, color = '#c76a48', alpha = 1, )
+ax2.set_xlabel('Age of a person', fontdict = {'font':'Serif', 'color': 'black', 'size': 16,'weight':'bold' })
+ax2.text(-17,0.025,'Age Distribution', {'font':'Serif', 'color': 'black','weight':'bold','size':24}, alpha = 0.9)
+ax2.text(-17,0.021, 'From this graph we have adult population is the median group.', 
+        {'font':'Serif', 'size':'16','color': 'black'})
+ax2.text(80,0.019, 'Total',{'font':'Serif', 'size':'14','color': '#c76a48','weight':'bold'})
+ax2.text(92,0.019, '=',{'font':'Serif', 'size':'14','color': 'black','weight':'bold'})
+ax2.text(97,0.019, 'Stroke',{'font':'Serif', 'size':'14','color': '#343bfe','weight':'bold'})
+ax2.text(113,0.019, '+',{'font':'Serif', 'size':'14','color': 'black','weight':'bold'})
+ax2.text(117,0.019, 'Healthy',{'font':'Serif', 'size':'14','color': '#e6a129','weight':'bold'})
+
+
+# plot the distribution plots and add legend and comments to the graph
+
+
+sns.kdeplot(data = dat[dat['stroke'] == 0], x = 'age',ax = ax3, shade = True,  alpha = 1, color = '#e6a129' )
+sns.kdeplot(data = dat[dat['stroke'] == 1], x = 'age',ax = ax3, shade = True,  alpha = 0.8, color = '#343bfe')
+
+ax3.set_xlabel('Age of a person', fontdict = {'font':'Serif', 'color': 'black', 'weight':'bold','size': 16})
+
+ax3.text(-17,0.0525,'Stroke-Age Distribution', {'font':'Serif', 'weight':'bold','color': 'black', 'size':24}, alpha= 0.9)
+ax3.text(-17,0.043,'From the Distribution plot it is clear that old people are \nhaving larger number of strokes than young people.', {'font':'Serif', 'color': 'black', 'size':14})
+ax3.text(100,0.043, 'Stroke ', {'font': 'Serif','weight':'bold','Size': '16','weight':'bold','style':'normal', 'color':'#343bfe'})
+ax3.text(117,0.043, '|', {'color':'black' , 'size':'16', 'weight': 'bold'})
+ax3.text(120,0.043, 'Healthy', {'font': 'Serif','weight':'bold', 'Size': '16','style':'normal', 'weight':'bold','color':'#e6a129'})
+
+fig.text(0.25,0.05,'Relationship between Heart Strokes and Age',{'font':'Serif', 'weight':'bold','color': 'black', 'size':30})
+plt.tight_layout()
+plt.savefig("Heart stroke and age")
+
